72acdd2ce42cb6c9d1b583af6a00d5e450639322
Pedro Henrique <gpedroemail@gmail.com>
2021-08-31 01:28:38 -0300

    Ajuste no serviço de manipulação das tarefas

--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -8,35 +8,38 @@ import { Task } from '../models/Task';
 export class TaskService {
 
   public lastID = 1;
-  tasks: Task[] = [{
-    id: 1,
-    title: "CRUD para processo seletivo",
-    description: "Realizado com foco em UI/UX. Obrigado pela oportunidade.",
-    finished: true,
-    created: new Date()
-  }];
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: "CRUD para processo seletivo",
+      description: "Realizado com foco em UI/UX. Obrigado pela oportunidade.",
+      finished: true,
+      created: new Date()
+    }
+  ];
 
   constructor() { }
 
   create(task: Task): Observable<Task> {
     this.tasks.push(task);
-    console.log(this.tasks);
     return of(task);
   }
 
   retrieve(): Observable<Task[]> {
-    console.log(this.tasks);
-
     return of(this.tasks);
   }
 
-  update(id: number, task: Task): Observable<Task> {
+  update(task: Task): Observable<Task> {
+    const index = this.tasks.map(function (e) { return e.id; }).indexOf(task.id);
+    if (index) {
+      this.tasks[index] = task;
+    }
     return of(task);
   }
 
   delete(task: Task): Observable<Task> {
-    const index = this.tasks.map(function(e) { return e.id; }).indexOf(task.id);
-    if(index) {
+    const index = this.tasks.map(function (e) { return e.id; }).indexOf(task.id);
+    if (index) {
       this.tasks.splice(index, 1);
     }
     return of(task);
